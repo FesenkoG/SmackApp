bc53a43f6160a0b077369adb557a142aef593afd
Georgy <fesenko.g@gmail.com>
2018-01-08 12:28:01 +0300

    Gradient

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Георгий Фесенко on 08.01.2018.
+//  Copyright © 2018 Georgy. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
