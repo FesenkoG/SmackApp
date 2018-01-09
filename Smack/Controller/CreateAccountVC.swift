00924df41c937dd54e1b812fd1bd4f4611462799
Georgy <fesenko.g@gmail.com>
2018-01-10 00:03:17 +0300

    Another commit

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -12,11 +12,14 @@ class CreateAccountVC: UIViewController {
 
     //Outlets
     @IBOutlet weak var usernameTxt: UITextField!
-    
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passTxt: UITextField!
-    
     @IBOutlet weak var userImg: UIImageView!
+    
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,15 +37,23 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        
+        guard let name = usernameTxt.text, usernameTxt.text != "" else {return}
         guard let email = emailTxt.text, emailTxt.text != "" else {return}
         guard let pass = passTxt.text, passTxt.text != "" else {return}
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                AuthService.instance.registerUser(email: email, password: pass, completion: { (success) in
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     
                     if success {
-                        print("logged in user", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.email)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -2,8 +2,8 @@
 //  AuthService.swift
 //  Smack
 //
-//  Created by Георгий Фесенко on 08.01.2018.
-//  Copyright © 2018 Georgy. All rights reserved.
+//  Created by Jonny B on 7/17/17.
+//  Copyright © 2017 Jonny B. All rights reserved.
 //
 
 import Foundation
@@ -11,11 +11,12 @@ import Alamofire
 import SwiftyJSON
 
 class AuthService {
+    
     static let instance = AuthService()
     
     let defaults = UserDefaults.standard
     
-    var isLoggedIn: Bool {
+    var isLoggedIn : Bool {
         get {
             return defaults.bool(forKey: LOGGED_IN_KEY)
         }
@@ -52,6 +53,7 @@ class AuthService {
         ]
         
         Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
+            
             if response.result.error == nil {
                 completion(true)
             } else {
@@ -61,7 +63,8 @@ class AuthService {
         }
     }
     
-    func loginUser(email: String, password: String, comletion: @escaping CompletionHandler) {
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
         let lowerCaseEmail = email.lowercased()
         
         let body: [String: Any] = [
@@ -70,30 +73,68 @@ class AuthService {
         ]
         
         Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
-            if response.result.error != nil {
-//                if let json = response.result.value as? Dictionary<String, Any> {
-//                    if let email = json["user"] as? String {
-//                        self.userEmail = email
-//                    }
-//                    if let token = json["token"] as? String {
-//                        self.authToken = token
-//                    }
-//                }
-                
-                //Using SwiftyJSON
-                
+            
+            if response.result.error == nil {
                 guard let data = response.data else { return }
                 let json = try! JSON(data: data)
                 self.userEmail = json["user"].stringValue
                 self.authToken = json["token"].stringValue
                 
                 self.isLoggedIn = true
-                
+                completion(true)
             } else {
-                comletion(false)
+                completion(false)
                 debugPrint(response.result.error as Any)
             }
         }
     }
     
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        
+        let header = [
+            "Authorization":"Bearer \(AuthService.instance.authToken)",
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                let json = try! JSON(data: data)
+                let id = json["_id"].stringValue
+                let color = json["avatarColor"].stringValue
+                let avatarName = json["avatarName"].stringValue
+                let email = json["email"].stringValue
+                let name = json["name"].stringValue
+                
+                UserDataService.instance.setUserData(id: id, avatarColor: color, avatarName: avatarName, email: email, name: name)
+                completion(true)
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
