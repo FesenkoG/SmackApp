4f637813fd5b5a0ddb522108b0bc9833001de51f
Georgy <fesenko.g@gmail.com>
2018-01-11 13:17:57 +0300

    Avatar Picker is done

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -24,11 +24,18 @@ class CreateAccountVC: UIViewController {
         super.viewDidLoad()
 
     }
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
     
     @IBAction func pickBgColorPressed(_ sender: Any) {
         
     }
     @IBAction func pickAvatarPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
         
     }
     
